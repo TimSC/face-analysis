ab214b1513504e44b7544d238fe9170d12bad400
Tim Sheerman-Chase <t.sheerman-chase@surrey.ac.uk>
2012-08-24 17:09:45 +0100

    A few minor fixes to variables

--- pyaam/createappearancemodel.py
+++ pyaam/createappearancemodel.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
 	appModel, appPcaSpace = pcaappearance.CalcApperanceModel(imageData, pix.shape)
 	
 	pickle.dump(appModel, open("appmodel.dat","wb"), protocol =  pickle.HIGHEST_PROTOCOL)
-	pickle.dump(appPcaSpace, open("appmodel.dat","wb"), protocol =  pickle.HIGHEST_PROTOCOL)
+	pickle.dump(appPcaSpace, open("apppcaspace.dat","wb"), protocol =  pickle.HIGHEST_PROTOCOL)
 
 	#print appModel.variances
 	#plt.plot(appModel.variances)
