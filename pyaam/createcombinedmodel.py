4bbca035495ec02febe1ba6203001b8d0a934fe8
Tim Sheerman-Chase <t.sheerman-chase@surrey.ac.uk>
2012-08-24 17:12:03 +0100

    A few minor fixes to variables

--- pyaam/createcombinedmodel.py
+++ pyaam/createcombinedmodel.py
@@ -3,6 +3,7 @@ import pcaappearance, pcashape, pickle
 
 class CombinedModel:
 	def __init__(self):
+		pass
 		#self.shapeModel = shapeModel
 		#self.appModel = appModel
 
@@ -11,8 +12,11 @@ if __name__ == "__main__":
 	shapeModel = pickle.load(open("shapemodel.dat","rb"))
 	appModel = pickle.load(open("appmodel.dat","rb"))
 
-	
+	shapePcaSpace = pickle.load(open("shapepcaspace.dat","rb"))
+	appPcaShape = pickle.load(open("apppcaspace.dat","rb"))	
 
+	print shapePcaSpace.shape
+	print appPcaShape.shape
 
 
 	combModel = CombinedModel()
