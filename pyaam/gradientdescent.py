680a1c58618935177b8c9ce5fa94124e877a03a4
Tim Sheerman-Chase <t.sheerman-chase@surrey.ac.uk>
2012-08-27 19:05:10 +0100

    Ignore area outside of synthetic face in difference

--- pyaam/gradientdescent.py
+++ pyaam/gradientdescent.py
@@ -14,23 +14,43 @@ def DifferenceIm(im1, im2):
 	outIm = np.array(diff, dtype=np.uint8)
 	return Image.fromarray(outIm)	
 
+evalCount = 0
+
 def Eval(eigVec, targetImg, combinedModel):
 	print eigVec
+	global evalCount
 
 	#Warp target image and get shape free face
 	im2 = combinedModel.TransformImageToNormalisedFace(targetImg, eigVec)
-	im2.show()
+	#im2.show()
+	im2.save("warped"+str(evalCount)+".jpg")
 
 	#Generate shape free appearance from eigenvector
 	im1 = combinedModel.GenerateFace(eigVec[5:])
-	im1.show()
+	#im1.show()
+	im1.save("synth"+str(evalCount)+".jpg")
 
 	#Calculate difference
 	arr1 = np.asarray(im1)
 	arr2 = np.asarray(im2)
 	diff = arr1 - arr2
-	print np.abs(diff).mean()
-	return np.abs(diff).reshape(diff.size).mean()
+	
+	#Ignore areas in synthetic appearance that are black
+	filteredDiff = []
+	for i in range(arr1.shape[0]):
+		for j in range(arr1.shape[1]):
+			black = True
+			for k in range(arr1.shape[2]):
+				if arr1[i,j,k] != 0:
+					black = False
+			if not black:
+				filteredDiff.append(diff[i,j,k])
+	filteredDiff = np.array(filteredDiff)
+
+	evalCount += 1
+	meanDiff = np.abs(filteredDiff).mean()
+	print meanDiff
+	return meanDiff
 
 if __name__ == "__main__":
 	combinedModel = pickle.load(open("combinedmodel.dat","rb"))
@@ -44,10 +64,10 @@ if __name__ == "__main__":
 
 	#DifferenceIm(im, test).show()
 	initial = np.zeros((10,))
-	initial[0] = 550. #Horizontal position
-	initial[1] = 570. #Vertical position
-	initial[2] = 300. #Scale
-	initial[2] = 0. #Rotation
+	initial[0] = 510. #Horizontal position
+	initial[1] = 695. #Vertical position
+	initial[2] = 240. #Scale
+	initial[3] = 0. #Rotation
 
 	#Fit model to target image by gradient descent
 	result = opt.fmin_powell(Eval, initial, args = (targetImg, combinedModel))
