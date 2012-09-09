9ae70b974454607c67e7febd4eab4cdac6248b9e
Tim Sheerman-Chase <t.sheerman-chase@surrey.ac.uk>
2012-09-09 15:44:32 +0100

    Start to evaluate AAM

--- pyaam/applyaam.py
+++ pyaam/applyaam.py
@@ -13,4 +13,32 @@ if __name__ == "__main__":
 	#Load prediction model
 	predictors = pickle.load(open("predictors.dat", "rb"))
 
+	#For each training image
+	frameNum = 0
+	framePos, im = posData[frameNum], pics[frameNum]
+
+	#Get shape free face
+	shapefree = combinedModel.ImageToNormaliseFace(im, framePos)
+
+	#Convert normalised face and shape to combined model eigenvalues
+	vals = combinedModel.NormalisedFaceAndShapeToEigenVec(shapefree, framePos)
+
+	#Purturb
+	horizonalSamples = [pt[0] for pt in framePos]
+	horizontalRange = max(horizonalSamples) - min(horizonalSamples)
+	offsetExample = -0.2
+	
+	changedVals = np.copy(vals)
+	perturb = np.zeros(changedVals.shape)	
+	perturb[0] = offsetExample * horizontalRange
+	changedVals = changedVals + perturb
+
+	print changedVals[0], vals[0]
+
+	#Reconstruct synthetic image
+	synthApp, synthShape = combinedModel.EigenVecToNormFaceAndShape(changedVals)
+
+	#Get norm face from source, based on perturbed shape
+	perturbSource = combinedModel.ImageToNormaliseFace(im, synthShape)
+
 	
